79b21b12032436a1199cf11215dff3ac0410f131
andreyRyabchikov <45429367+andreyRyabchikov@users.noreply.github.com>
2022-11-28 16:39:32 +0300

    +servises

--- chat.Services/MapperProfile/ServicesProfile.cs
+++ chat.Services/MapperProfile/ServicesProfile.cs
@@ -8,11 +8,43 @@ public class ServicesProfile : Profile
 {
     public ServicesProfile()
     {
-        #region Users
-
+      #region Student
         CreateMap<User, UserModel>().ReverseMap();
-        CreateMap<User, UserPreviewModel>().ReverseMap();  
+        CreateMap<User, UserPreviewModel>().ReverseMap();
+        #endregion
+
+             #region Chats
+
+       CreateMap<Chat, ChatModel>().ReverseMap();
+        CreateMap<Chat, ChatPreviewModel>().ReverseMap();
+        #endregion
+         #region ChatMembers
+
+        CreateMap<ChatMember, ChatMemberModel>().ReverseMap();
+         CreateMap<ChatMember, ChatPreviewModel>().ReverseMap();
+        #endregion
+         #region BlackList
+
+        CreateMap<BlackList, BlackListModel>().ReverseMap();
+        #endregion
+         #region Attachments
+
+        CreateMap<Attachment, AttachmentModel>().ReverseMap();
+         CreateMap<Attachment, AttachmentPreviewModel>().ReverseMap();
+        #endregion
+        #region Contact
+
+         CreateMap<Contact, ContactModel>().ReverseMap();
+        #endregion
+
+        #region Messages
+
+         CreateMap<Message, MessageModel>().ReverseMap();
+         CreateMap<Message, MessagePreviewModel>().ReverseMap();
+
 
         #endregion
+
+
     }
 }
